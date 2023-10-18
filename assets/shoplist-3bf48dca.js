a3b796f3775822b29f38c6846abdf6f944a1d17c
arteeeemt <128076184+arteeeemt@users.noreply.github.com>
2023-10-18 16:22:46 +0000

    Deploying to gh-pages from @ arteeeemt/project_12@8e89b3f8c6ea0ac1a4cc4e2ea9fa6421aa6a54a6 🚀

--- assets/shoplist-3bf48dca.js
+++ assets/shoplist-3bf48dca.js
@@ -1,13 +1,13 @@
-import"./support-13a0f787.js";const s=document.querySelector(".js-shoping-list-book"),l=document.querySelector(".listOfBookFromCategory");let o=JSON.parse(localStorage.getItem("books"));e();s.addEventListener("click",p);function e(){if(!o||o.length===0){const i=a();l.innerHTML=i,s.style.display="none";return}s.style.display="flex",s.innerHTML=r(o)}function p(i){if(i.target.tagName!="BUTTON")return;const t=o.findIndex(n=>n._id===i.target.dataset.id);o.splice(t,1),localStorage.setItem("books",JSON.stringify(o)),e()}function a(){return` <div class="shoping-empty">
+import"./support-00994241.js";const o=document.querySelector(".js-shoping-list-book"),p=document.querySelector(".listOfBookFromCategory");let i=JSON.parse(localStorage.getItem("books"));e();o.addEventListener("click",r);function e(){if(!i||i.length===0){const s=l();p.innerHTML=s,o.style.display="none";return}o.style.display="flex",o.innerHTML=a(i)}function r(s){if(s.target.tagName!="BUTTON")return;const t=i.findIndex(n=>n._id===s.target.dataset.id);i.splice(t,1),localStorage.setItem("books",JSON.stringify(i)),e()}function l(){return` <div class="shoping-empty">
       <p class="shop-list-empty-text">
         This page is empty, add some books and proceed to order.
       </p>
-      <div class="shopping-list-empty-img"></div></div>`}function r(i){return i.map(t=>`
+      <div class="shopping-list-empty-img"></div></div>`}function a(s){return s.map(t=>`
                <li class ="shoping-books-item">
                  <img src="${t.book_image}" alt="${t.title}" class="shopping-img-card">
                  <div>
                   <svg class="shopping-card-button" data-id="${t._id}" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
-                   <ellipse cx="14" cy="14" rx="14" ry="14" fill="#4F2EE8"/>
+                   <ellipse cx="14" cy="14" rx="14" ry="14" fill="#4F2EE8"/><use href="/img/sprite.svg#icon-trash"></use>
                  </svg>
                  <h2 class = "shopping-card-title">${t.title}</h2>
                 
